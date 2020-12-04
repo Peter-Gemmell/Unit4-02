0d1caad9ff12c023b687117be4eab6a1c7384433
Peter Gemmell <peter.gemmell@ocsbstudent.ca>
2020-12-04 15:59:54 +0000

    Initial commit

---
+++ script.js
@@ -0,0 +1,19 @@
+function myFunction() {
+  document.getElementById("demo").innerHTML  = "Now thats a good cup joe"
+}
+
+function myFunction() {
+  var x= document.createElement("IMG");
+  x.setAttribute("src", "image/Peter.jpg");
+  x.setAttribute("width", "200");
+  x.setAttribute("height","200");
+  x.setAttribute("alt", "Peter");
+  document.body.appendChild(x);
+}
+
+window.onclick = myFunction;
+
+function myFunction() {
+  document.getElementsByTagName("BODY")[0].style.backgroundColor = "yellow";
+}
+
