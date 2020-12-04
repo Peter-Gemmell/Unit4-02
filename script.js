bf608afb0259996538eb3514db27643c81af62d8
Peter Gemmell <peter.gemmell@ocsbstudent.ca>
2020-12-04 16:05:57 +0000

    how am i supposed to know that i am supposed to have function 1 or 2 or 3 and not just myfunction i just took half 3/4th of the fucking class to discover this fucking  bullshit

--- script.js
+++ script.js
@@ -1,8 +1,8 @@
-function myFunction() {
+function myFunction1() {
   document.getElementById("demo").innerHTML  = "Now thats a good cup joe"
 }
 
-function myFunction() {
+function myFunction2() {
   var x= document.createElement("IMG");
   x.setAttribute("src", "image/Peter.jpg");
   x.setAttribute("width", "200");
@@ -11,9 +11,9 @@ function myFunction() {
   document.body.appendChild(x);
 }
 
-window.onclick = myFunction;
+window.onclick = myFunction3;
 
-function myFunction() {
+function myFunction3() {
   document.getElementsByTagName("BODY")[0].style.backgroundColor = "yellow";
 }
 
